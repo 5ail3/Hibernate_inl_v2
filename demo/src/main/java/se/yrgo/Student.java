4e2f8d559b0a62cbc72331e82d66ec434b36c6ba
elisan0107 <elisan0107@skola.goteborg.se>
2024-08-05 20:20:00 +0200

    finishing class Student

--- demo/src/main/java/se/yrgo/Student.java
+++ demo/src/main/java/se/yrgo/Student.java
@@ -8,4 +8,43 @@ import jakarta.persistence.ManyToOne;
 
 public class Student {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @ManyToOne
+    private Tutor tutor;
+
+    // Constructors
+    public Student() {}
+
+    public Student(String name) {
+        this.name = name;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Tutor getTutor() {
+        return tutor;
+    }
+
+    public void setTutor(Tutor tutor) {
+        this.tutor = tutor;
+    }
 }
