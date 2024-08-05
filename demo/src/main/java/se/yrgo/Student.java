3cb34aac5a031068bd1f1c80053470cfe6d3a439
elisan0107 <elisan0107@skola.goteborg.se>
2024-08-05 20:18:49 +0200

    adding class student + imports

---
+++ demo/src/main/java/se/yrgo/Student.java
@@ -0,0 +1,11 @@
+package se.yrgo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+public class Student {
+
+}
