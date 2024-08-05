21e6c1f810a4daa02ed69740e2170a64a5a50374
elisan0107 <elisan0107@skola.goteborg.se>
2024-08-05 20:20:43 +0200

    finishing class Tutor

---
+++ demo/src/main/java/se/yrgo/Tutor.java
@@ -0,0 +1,53 @@
+package se.yrgo;
+
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Tutor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "tutor", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Student> students = new ArrayList<>();
+
+    // Constructors
+    public Tutor() {}
+
+    public Tutor(String name) {
+        this.name = name;
+    }
+
+    // Getters and setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+        student.setTutor(this);
+    }
+}
